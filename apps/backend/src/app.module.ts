644b41addae594367f1a9c3ce5ff3315e114a69f
Tan Tiong Guan <tantiongguan123@gmail.com>
2024-08-29 22:36:18 +0800

    chore: guard all routes by default

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { ServerStatusModule } from '@backend/features/server-status/server-status.module';
+import { APP_GUARD } from '@nestjs/core';
 import { DatabaseConfigurationService } from './config/typeorm.config';
 import { AuthenticationModule } from './features/auth/auth.module';
+import { JwtAuthGuard } from './features/auth/guards/jwt-auth.guard';
 import { MemberModule } from './features/member/member.module';
 
 @Module({
@@ -14,6 +16,12 @@ import { MemberModule } from './features/member/member.module';
     ServerStatusModule,
     AuthenticationModule,
     MemberModule,
+  ],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: JwtAuthGuard,
+    },
   ],
 })
 export class AppModule {}
