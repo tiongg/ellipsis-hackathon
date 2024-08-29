be15c6c9372517bccba3d641940ec2684e4d3a77
Tan Tiong Guan <tantiongguan123@gmail.com>
2024-08-29 21:55:25 +0800

    chore: remove react-query, add swr

--- apps/frontend/app/Providers.tsx
+++ apps/frontend/app/Providers.tsx
@@ -1,44 +1,7 @@
-//https://tanstack.com/query/latest/docs/framework/react/guides/advanced-ssr
 'use client';
 
 import { PropsWithChildren } from 'react';
-import {
-  isServer,
-  QueryClient,
-  QueryClientProvider,
-} from '@tanstack/react-query';
-
-function makeQueryClient() {
-  return new QueryClient({
-    defaultOptions: {
-      queries: {
-        // With SSR, we usually want to set some default staleTime
-        // above 0 to avoid refetching immediately on the client
-        staleTime: 60 * 1000,
-      },
-    },
-  });
-}
-
-let browserQueryClient: QueryClient | undefined = undefined;
-
-function getQueryClient() {
-  if (isServer) {
-    return makeQueryClient();
-  } else {
-    if (!browserQueryClient) browserQueryClient = makeQueryClient();
-    return browserQueryClient;
-  }
-}
 
 export default function Providers({ children }: PropsWithChildren) {
-  // NOTE: Avoid useState when initializing the query client if you don't
-  //       have a suspense boundary between this and the code that may
-  //       suspend because React will throw away the client on the initial
-  //       render if it suspends and there is no boundary
-  const queryClient = getQueryClient();
-
-  return (
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
-  );
+  return <>{children}</>;
 }
