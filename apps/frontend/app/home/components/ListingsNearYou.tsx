a977dcf4e3a51dd87528678c9f985aff7b9bee39
Tan Tiong Guan <tantiongguan123@gmail.com>
2024-08-30 16:01:00 +0800

    feat: add listings near me

---
+++ apps/frontend/app/home/components/ListingsNearYou.tsx
@@ -0,0 +1,53 @@
+import {
+  DisplayListingDto,
+  GetListingDto,
+} from '@shared-types/features/listing/get-listing.dto';
+import _ from 'lodash';
+import useSWR from 'swr';
+
+import { Card } from '@components/card';
+
+/**
+ * Single listing item containing details + quantity
+ *
+ * Assumes every product in the array is the same!
+ */
+function IndividualListing({
+  productListing,
+}: {
+  productListing: DisplayListingDto[];
+}) {
+  const product = productListing[0].product;
+  const quantity = productListing.length;
+
+  return (
+    <Card className="w-[350px]">
+      <img src={product.productImageUrl} alt={product.productName} />
+      <p>{product.productName}</p>
+      <p>Quantity: {quantity}x</p>
+      <p>From: {product.store.storeName}</p>
+    </Card>
+  );
+}
+
+export default function ListingsNearYou() {
+  const { data: listings, isLoading: isLoadingListings } =
+    useSWR<GetListingDto>('/api/listing');
+
+  if (isLoadingListings) {
+    return <p>Loading...</p>;
+  }
+
+  //Group products together and count
+  //TODO: why is this not done on the be???
+  const groupedListings = _.groupBy(listings, (listing) => listing.productId);
+
+  return (
+    <div>
+      <p>Current listings:</p>
+      {Object.entries(groupedListings).map(([productId, productListing]) => (
+        <IndividualListing key={productId} productListing={productListing} />
+      ))}
+    </div>
+  );
+}
