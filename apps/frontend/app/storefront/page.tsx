8915f7286fcad8fcbc9ffe5e9e61feba26828ff1
Tan Tiong Guan <tantiongguan123@gmail.com>
2024-08-31 13:55:48 +0800

    feat: add toggle for dashboard and storefront

--- apps/frontend/app/storefront/page.tsx
+++ apps/frontend/app/storefront/page.tsx
@@ -1,11 +1,20 @@
 'use client';
 
+import { useState } from 'react';
 import { GetStorefrontsDto } from '@shared-types/features/storefront/get-storefronts.dto';
 import Link from 'next/link';
 import useSWR from 'swr';
+import { match } from 'ts-pattern';
 
+import { Button } from '@components/button';
 import FullScreenCenter from '../../components/FullScreenCenter';
 import DashboardPage from './components/Dashboard';
+import StorefrontPage from './components/StorefrontPage';
+
+enum StorefrontTab {
+  DASHBOARD = 'DASHBOARD',
+  PRODUCTS = 'PRODUCTS',
+}
 
 /**
  * Main dashboard for storefront.
@@ -14,9 +23,14 @@ import DashboardPage from './components/Dashboard';
  */
 export default function Storefront() {
   const { data, isLoading } = useSWR<GetStorefrontsDto>('/api/storefront');
+  const [activeTab, setActiveTab] = useState<StorefrontTab>(
+    StorefrontTab.DASHBOARD
+  );
+
   if (isLoading || !data) {
     return <FullScreenCenter>Loading...</FullScreenCenter>;
   }
+
   if (data.length <= 0) {
     return (
       <FullScreenCenter>
@@ -34,9 +48,28 @@ export default function Storefront() {
       <div className="flex gap-4 flex-col p-4">
         <div className="flex justify-between">
           <p className="font-bold text-2xl">Store: {storefront.storeName}</p>
+          <Button
+            onClick={() =>
+              setActiveTab(
+                match(activeTab)
+                  .with(StorefrontTab.DASHBOARD, () => StorefrontTab.PRODUCTS)
+                  .with(StorefrontTab.PRODUCTS, () => StorefrontTab.DASHBOARD)
+                  .exhaustive()
+              )
+            }
+          >
+            {match(activeTab)
+              .with(StorefrontTab.DASHBOARD, () => 'Products')
+              .with(StorefrontTab.PRODUCTS, () => 'Dashboard')
+              .exhaustive()}
+          </Button>
         </div>
-        <DashboardPage />
-        {/* <StorefrontPage storeId={storefront.storeId} /> */}
+        {match(activeTab)
+          .with(StorefrontTab.DASHBOARD, () => <DashboardPage />)
+          .with(StorefrontTab.PRODUCTS, () => (
+            <StorefrontPage storeId={storefront.storeId} />
+          ))
+          .exhaustive()}
       </div>
     </FullScreenCenter>
   );
